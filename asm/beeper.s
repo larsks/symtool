dbcabda18c8b1ac6b3b6a722514330a8622cb3d0
Lars Kellogg-Stedman <lars@oddbit.com>
2021-02-02 19:41:23 -0500

    modify beeper to exit on keypress

--- asm/beeper.s
+++ asm/beeper.s
@@ -1,11 +1,17 @@
+; beep periodically until a key is pressed
+;
         .include        "monitor.s"
 
 main:   ldx             #$ff
         ldy             #$ff
+
 loop:   dex                             ; for y in range(0xff):
         bne             loop            ;    for x in range(0xff):
         dey                             ;        pass
         bne             loop
 
         jsr             BEEP
-        jmp             main
+        clc
+        jsr             KYSTAT
+        bcc             main
+        rts
